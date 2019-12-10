acbcf7a2bc3b784cd8d05cb43d8259b342070b92
KarolinaAntonowicz <karola.antonowicz@gmail.com>
2019-12-10 18:36:17 +0100

    add loop begining

--- analizator_file.py
+++ analizator_file.py
@@ -3,5 +3,18 @@ menu = ['1. Pobierz plik z internetu', '2. Zlicz liczbę liter w pobranym pliku'
         '6. Wygeneruj raport o użyciu liter (A-Z)', '7. Zapisz statystyki z punktów 2-5 do pliku statystyki.txt',
         '8. Wyjście z programu']
 isWorking = True
-
+status_code = 200
 text = list()
+
+
+
+
+
+
+while(isWorking):
+    for element in menu:
+        print(element)
+    action = int(input())
+
+
+      
