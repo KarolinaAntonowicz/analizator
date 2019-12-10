3f3755d82923c3c685e961e068b48e6e04646f81
KarolinaAntonowicz <karola.antonowicz@gmail.com>
2019-12-10 20:25:12 +0100

    add menu and status code

---
+++ analizator.py
@@ -0,0 +1,10 @@
+menu = ['1. Download the file from the internet', '2. Count the number of letters in the downloaded file',
+        '3. Count the number of words in the file',
+        '4. Count the number of punctuation marks in the file.', '5. Count the number of sentences in the file',
+        '6. Generate a letter usage report (A-Z)', '7. Save the statistics from points 2-5 to a statystyki.txt file',
+        '8. Exit the program']
+isWorking = True
+status_code = 200
+text = list()
+
+
