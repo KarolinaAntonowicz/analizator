6a09271fd692f2e1d5c25eebd8b34f819d9b4a8a
KarolinaAntonowicz <karola.antonowicz@gmail.com>
2019-12-10 18:30:13 +0100

    add menu

---
+++ analizator_file.py
@@ -0,0 +1,7 @@
+menu = ['1. Pobierz plik z internetu', '2. Zlicz liczbę liter w pobranym pliku', '3. Zlicz liczbę wyrazów w pliku',
+        '4. Zlicz liczbę znaków interpunkcyjnych w pliku.', '5. Zlicz liczbę zdań w pliku',
+        '6. Wygeneruj raport o użyciu liter (A-Z)', '7. Zapisz statystyki z punktów 2-5 do pliku statystyki.txt',
+        '8. Wyjście z programu']
+isWorking = True
+
+text = list()
